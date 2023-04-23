0b5395ebd8c0ce6ef6cbed3d3ab33ff4175dd069
“asensanjula” <asensanjula@gmail.com>
2023-04-23 16:45:39 +0800

    feat(): add header and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './Footer.scss';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div>PappzFlix</div>
+      <div>@2023, PappzFlix, Inc.</div>
+    </div>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import './Home.scss'
+import { MovieListing } from '../MovieListing/MovieListing';
+import MovieApi from '../../common/apis/MovieApi';
+import { APIKey } from '../../common/apis/MovieApiKey';
 
 const Home = () => {
+
+  useEffect( ()=> {
+    let movieText = "harry";
+    const fetchMovies = async () => {
+      const response = await MovieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+      .catch((err) => {
+        console.log(err);
+      });
+
+      console.log("The response for Movie API ", response);
+    }
+    fetchMovies();
+  }, []);
+
   return (
-    <div>Home</div>
+    <>
+      <div className='banner'></div>
+      <MovieListing/>
+    </>
   )
 }
 
