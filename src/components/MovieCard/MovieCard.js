421422da693b07ea7c453cb5e57f1cd2d061d6f4
“asensanjula” <asensanjula@gmail.com>
2023-04-24 11:19:32 +0800

    feat(): add movie lisiting screen

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const MovieCard = () => {
+export const MovieCard = ({movie}) => {
   return (
-    <div>MovieCard</div>
+    <div>
+      {movie?.Title}
+    </div>
   )
 }
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { getAllMovies } from '../../features/Movies/MovieSlice'
+import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../features/Movies/MovieSlice";
+import { MovieCard } from "../MovieCard/MovieCard";
 
 export const MovieListing = () => {
+  const movies = useSelector(getAllMovies);
+  console.log("list ",movies);
+  console.log("val ",movies.Response === "True");
 
-    const movies = useSelector(getAllMovies);
-    console.log(movies);
+  let renderMovies =
+    movies.Response === "True" ? 
+      movies?.Search?.map((movie, index) => (
+        (<MovieCard key={index} movie={movie} />)
+      ))
+     : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+    
+    console.log(renderMovies);
   return (
-    <div>MovieListing</div>
-  )
-}
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">
+          {renderMovies}
+        </div>
+      </div>
+    </div>
+  );
+};
